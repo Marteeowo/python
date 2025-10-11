31b33ae1762c86bc9c42ce36669fee4d5e4a6ca7
Eri <matu19kuusk@gmail.com>
2025-10-11 11:55:13 +0300

    h01

---
+++ h01.py
@@ -0,0 +1,54 @@
+#Marten-Jürgen Kuusk ITS25 09.10.2025
+import turtle
+
+# #Kolmnurk
+# turtle.penup() #pliiats üles
+# turtle.goto(-400, 200) #liigub asukohta
+# turtle.pendown() #pliiats alla
+# 
+# for i in range(3): #cycle
+#     turtle.fd(200) #forward
+#     turtle.lt(120) #left
+
+#Minu eesnime täht
+turtle.penup() #pliiats üles
+turtle.goto(-100, 200) #liigub asukohta
+turtle.pendown() #pliiats alla
+
+
+
+
+turtle.fd(50)
+turtle.lt(90)
+turtle.fd(200)
+turtle.lt(90)
+turtle.fd(50)
+turtle.lt(40)
+turtle.fd(40)
+turtle.rt(80)
+turtle.fd(40)
+turtle.lt(40)
+turtle.fd(50)
+turtle.lt(90)
+turtle.fd(200)
+turtle.lt(90)
+turtle.fd(50)
+turtle.lt(90)
+turtle.fd(120)
+turtle.rt(120)
+turtle.fd(35)
+turtle.lt(60)
+turtle.fd(35)
+turtle.rt(120)
+turtle.fd(120)
+
+# #Ruut
+# turtle.penup() #pliiats üles
+# turtle.goto(0, 0) #liigub asukohta
+# turtle.pendown() #pliiats alla
+# 
+# for i in range(4): #cycle
+#     turtle.fd(150) #forward
+#     turtle.lt(90) #left
+
+turtle.done()
