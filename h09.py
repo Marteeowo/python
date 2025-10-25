f6d1063fa679d33fb97f54533906b067e90ef988
Eri <matu19kuusk@gmail.com>
2025-10-25 14:00:39 +0300

    HELP

---
+++ h09.py
@@ -0,0 +1,11 @@
+#Harjutus09 MJ ITS25 25.10.25
+
+for i in range(1,21):
+    if i%15 == 0:
+        print(i,"TIKTAK")
+    elif i%3 == 0:
+        print(i,"TIK")
+    elif i%5 == 0:
+        print(i,"TAK")
+    else:
+        print(i)
