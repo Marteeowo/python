8f11bd7304555a9556e31031deef103562e76b33
Eri <matu19kuusk@gmail.com>
2025-11-03 20:49:08 +0200

    turtle kodutoo hindeline

---
+++ kodutoo02.py
@@ -0,0 +1,15 @@
+#Kodutoo02 MJ ITS25 03.11.25
+import turtle
+
+for i in range (4): #kordab kilpkonna k2ske 4 korda
+     turtle.fd(300) #liigutab kilpkonna edasi 300px
+     turtle.lt(90) #poorab kilpkonna vasakule 90 kraadi
+     turtle.fd(100)
+     turtle.lt(90)
+     turtle.fd(300)
+     turtle.lt(90)
+     turtle.fd(100)
+     for j in range (2): #kordab ymberpooramist kaks korda parast esimesi kaske
+          turtle.lt(90)
+          
+turtle.done()
